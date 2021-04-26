02a0a93f5165c2187b7085b84e78d0e737c5f0ec
Diego Rangel Piranga Costa <diegorang@outlook.com.br>
2021-04-26 14:41:01 -0300

    Create Permutations.cpp

---
+++ Introductory Problems/Permutations.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define all(x) x.begin(), x.end()
+#define sz(x) i(int) x.size()
+#define endl "\n"
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair <int, int> ii;
+typedef pair <ll, ll> pll;
+ 
+int main(){
+	int n;
+	cin >> n;
+	vector <int> ans, odd;
+	for(int i = 1; i <= n; i++){
+		if(!(i&1))
+			ans.push_back(i);
+		else
+			odd.push_back(i);
+	}
+	for(auto i : odd)
+		ans.push_back(i);
+	bool ok = true;
+	for(int i = 1; i < n and ok; i++){
+		//cout << ans[i] << " " << ans[i-1] << " " <<  abs(ans[i]-ans[i-1]) << endl;
+		ok &= abs(ans[i]-ans[i-1]) > 1;
+	}
+	if(ok){
+		for(auto i : ans)
+			cout << i << " ";
+		cout << endl;
+	}
+	else
+		cout << "NO SOLUTION" << endl;
+	return 0;
+}
