9784b1b5e48fc8d578a82201fa4007f7e7987a22
Diego Rangel Piranga Costa <diegorang@outlook.com.br>
2021-04-26 09:26:46 -0300

    Create ExponentiationII.cpp

---
+++ Mathematics/ExponentiationII.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define all(x) x.begin(), x.end()
+#define sz(x) i(int) x.size()
+#define endl "\n"
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair <int, int> ii;
+typedef pair <ll, ll> pll;
+ 
+const ll mod = 1e9+7;
+
+/*a^b^c mod m = a^(b^c mod n) mod m, where n = φ(m) Euler's totient function.
+If m is prime, then n = m-1.
+*/ 
+
+ll fexp(ll b, ll e, ll md){
+	ll ans = 1;
+	while(e){
+		if(e&1)
+			ans = (ans%md*b%md)%md;
+		e >>= 1;
+		b = (b%md*b%md)%md;
+	}
+	return ans%md;
+}
+ 
+int main(){
+	int n;
+	cin >> n;
+	while(n--){
+		ll a, b, c;
+		cin >> a >> b >> c;
+		cout << fexp(a, fexp(b, c, mod-1), mod)<< endl;
+	}
+	return 0;
+}
